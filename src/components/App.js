6c413258162474ecbc987397df5ad3d003a31758
Dharmik48 <hingudharmik4@gmail.com>
2021-07-09 20:36:18 +0530

    add search bar and got valur from it

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '1em' }}>
+        <SearchBar />
+      </div>
+    )
+  }
+}
+
+export default App
